030a03bcca723c51eb2a055a5062337bb1507680
Pavel <sn0oze@email.com>
2024-05-15 22:45:05 +0300

    text

--- tests/test_xauto_and_out.py
+++ tests/test_xauto_and_out.py
@@ -13,20 +13,23 @@ import allure
 def test_authorization_and_out(browser_headless):  # Тест проверяет авторизацию на сайте по email и выходит из аккаунта
     smart_food = Smart_food(browser_headless)
     smart_food.visit()
-    smart_food.register_button.click_force()
-    smart_food.login_email_modal_autorization.wait_and_click()
-    smart_food.user_login_input.send_keys(EMAIL)
-    smart_food.user_password_input.send_keys(PASSWORD)
+    with allure.step('Авторизация'):
+        smart_food.register_button.click_force()
+        smart_food.login_email_modal_autorization.wait_and_click()
+        smart_food.user_login_input.send_keys(EMAIL)
+        smart_food.user_password_input.send_keys(PASSWORD)
     smart_food.user_label_zapomnit_menya.wait_and_click()
     smart_food.user_login_button.wait_and_click()
     time.sleep(1)
-    smart_food.cabinet_menu_button.wait_and_hover()
-    cabinet_menu_button_title = smart_food.cabinet_menu_button.get_dom_attribute("Title")
-    assert cabinet_menu_button_title == "autotest testing"
-    smart_food.cabinet_menu_button_quit.click_force()
-    browser_headless.refresh()
-    cabinet_menu_button_title = smart_food.cabinet_menu_button.get_dom_attribute("Title")
-    assert cabinet_menu_button_title != "autotest testing"
+    with allure.step('Выход'):
+        smart_food.cabinet_menu_button.wait_and_hover()
+        cabinet_menu_button_title = smart_food.cabinet_menu_button.get_dom_attribute("Title")
+        assert cabinet_menu_button_title == "autotest testing"
+        smart_food.cabinet_menu_button_quit.click_force()
+    with allure.step('Проверка выхода'):
+        browser_headless.refresh()
+        cabinet_menu_button_title = smart_food.cabinet_menu_button.get_dom_attribute("Title")
+        assert cabinet_menu_button_title != "autotest testing"
 
 
 @pytest.mark.functional
