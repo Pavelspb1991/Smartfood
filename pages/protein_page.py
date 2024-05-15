7e1c5e80cdb821851b832c2f4e88a750b98d2cd8
Pavel <sn0oze@email.com>
2024-05-15 11:18:31 +0300

    text

---
+++ pages/protein_page.py
@@ -0,0 +1,29 @@
+from pages.base_page import BasePage
+from components.components import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class Protein_page(BasePage):
+
+    def __init__(self, driver):
+        self.base_url = 'https://smart-food.shop/catalog/dobavki-dlya-sportsmenov/proteiny/syvorotochnyy-protein/'
+        super().__init__(driver, self.base_url)
+        # Блок с кнопкапи сравнить, отложить и корзина
+        self.card_compare = WebElement(driver,
+                                       'div.catalog_block.items.row.margin0.has-bottom-nav.js_append.ajax_load.block.flexbox > div:nth-child(1)')
+        self.compare_button = WebElement(driver, '(//div/div[2]/div[1]/div[2]/span)[7]', 'xpath')
+        self.compare_modal_message = WebElement(driver, '#main > div.notice-surface.notice-surface--right')
+        self.compare_menu_count = WebElement(driver, '#header > div > div.logo_and_menu-row.with-search.header__top-part > div > div > div.right-icons.wb.header__top-item > div > div:nth-child(2) > a > span > span.count')
+        self.card_compare_2 = WebElement(driver, 'div.catalog_block.items.row.margin0.has-bottom-nav.js_append.ajax_load.block.flexbox > div:nth-child(2)')
+        self.compare_button_2 = WebElement(driver, '(//div/div[2]/div[1]/div[2]/span)[11]', 'xpath')
+        self.compare_menu = WebElement(driver, 'div.right-icons.wb.header__top-item > div > div:nth-child(2) > a')
+        self.delete_all_compare_batton = WebElement(driver, ' div.catalog-compare__top.flexbox.flexbox--row.justify-content-between.align-items-normal > span')
+        self.delete_one_card_button = WebElement(driver, '(//div/span)[32]', 'xpath')
+        self.text_compare_menu_empty = WebElement(driver, 'p > font')
+        self.checkbox_difference = WebElement(driver, '#compare_diff')
+        self.checkbox_difference_div = WebElement(driver, 'div > div.owl-item.active.current > div > div')
+        self.add_card_in_menu_compare_button = WebElement(driver,
+                                                          '(//div/div[1]/div[1]/div/div[2]/div/div/div/a)[3]', 'xpath')
+
+
+
---
+++ tests/test_compare_delete_buttons.py
@@ -0,0 +1,96 @@
+from pages.protein_page import Protein_page
+import time
+import allure
+import pytest
+
+
+@allure.description('Проверка  добавления и удаления товаров 2 товаров из сравнения')
+@allure.severity(allure.severity_level.NORMAL)
+def test_compare_button(browser):
+    protein_page = Protein_page(browser)
+    protein_page.visit()
+    protein_page.card_compare.exist()
+    protein_page.card_compare.wait_and_hover()
+    with allure.step('Нажать на кнопку сравнения 1 товар'):
+        protein_page.compare_button.click_force()
+    protein_page.compare_modal_message.wait_for_element_visible()
+    with allure.step('Проверить кликабельность модальное окно'):
+        assert protein_page.compare_modal_message.visible()
+    protein_page.compare_modal_message.click()
+    time.sleep(2)
+    protein_page.back()
+    time.sleep(2)
+    with allure.step('Проверить, что счетчик +1'):
+        assert protein_page.compare_menu_count.get_text() == '1'
+    with allure.step('Нажать на кнопку сравнения 2  карточки товара'):
+        protein_page.card_compare_2.wait_and_hover()
+        protein_page.compare_button_2.click()
+    time.sleep(2)
+    with allure.step('Проверить счетчик после 2 товара'):
+        assert protein_page.compare_menu_count.get_text() == '2'
+    protein_page.refresh()
+    protein_page.compare_menu.click()
+    with allure.step('Нажать на кнопку удалить все и проверить,что счетчик 0'):
+        protein_page.delete_all_compare_batton.click()
+        protein_page.refresh()
+        assert protein_page.compare_menu_count.get_text() == '0'
+
+
+@allure.description('Проверка  добавления и удаления товаров 2 товаров из сравнения,по одному')
+@allure.severity(allure.severity_level.NORMAL)
+def test_compare_button_2(browser_headless):
+    protein_page = Protein_page(browser_headless)
+    protein_page.visit()
+    protein_page.card_compare.exist()
+    protein_page.card_compare.wait_and_hover()
+    with allure.step('Нажать на кнопку сравнения 2 товаров'):
+        protein_page.compare_button.click()
+        protein_page.card_compare_2.wait_and_hover()
+        protein_page.compare_button_2.click()
+        protein_page.refresh()
+        protein_page.compare_menu.click()
+        protein_page.delete_one_card_button.wait_and_click()
+        time.sleep(2)
+        protein_page.delete_one_card_button.wait_and_click()
+    with allure.step('Обновляем страницу'):
+        protein_page.refresh()
+    with allure.step('Проверить, что cписок сравниваемых элементов пуст'):
+        assert protein_page.text_compare_menu_empty.get_text() == 'Список сравниваемых элементов пуст.'
+
+
+@allure.description('Проверка  чекбокса  Только отличия в меню сравнения')
+@allure.severity(allure.severity_level.NORMAL)
+def test_checkbox_compare(browser_headless):
+    protein_page = Protein_page(browser_headless)
+    protein_page.visit()
+    protein_page.card_compare.exist()
+    protein_page.card_compare.wait_and_hover()
+    with allure.step('Нажать на кнопку сравнения 2 товаров'):
+        protein_page.compare_button.click()
+        protein_page.card_compare_2.wait_and_hover()
+        protein_page.compare_button_2.click()
+        protein_page.refresh()
+        protein_page.compare_menu.click()
+        time.sleep(2)
+    with allure.step('Считаем количество div с опциями сравнения'):
+        div_before_click = protein_page.checkbox_difference_div.find_elements()
+    with allure.step('Клик на чебокс с разницей'):
+        protein_page.checkbox_difference.click_force()
+    time.sleep(2)
+    with allure.step('Считаем количество div с опциями после клика на чекбокс,должнно быть разное количество'):
+        assert div_before_click != protein_page.checkbox_difference_div.find_elements()
+
+
+@allure.description('Проверка  добавления товаров сравнения из меню сравнения')
+@allure.severity(allure.severity_level.NORMAL)
+def test_add_card_to_compare(browser_headless):
+    protein_page = Protein_page(browser_headless)
+    protein_page.visit()
+    protein_page.card_compare.exist()
+    with allure.step('Нажать на кнопку добавления товаров в сравнение'):
+        protein_page.card_compare.wait_and_hover()
+        protein_page.compare_button.click()
+    protein_page.refresh()
+    protein_page.compare_menu.click()
+    with allure.step('Нажать на кнопку добавления карточки в сравнение из меню сравнения'):
+        protein_page.add_card_in_menu_compare_button.click()
